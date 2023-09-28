f61b16cd09bd9f98c340742187cda3faa45d5295
Lbernardi <101919348+Fujcyuj@users.noreply.github.com>
2023-09-28 12:51:38 +0200

    .

---
+++ checkTelefono.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public static class Telefono
+{
+
+    public static string Check(string[] input)
+    {
+        /*if (input[0] == "3" && input.Length == 10)
+        { return input + ""; }
+        else if (input[0] == "+" && input[1] == "3" && input[2] == "9" && input.Length == 13)
+        { return input + ""; }
+        else if (input[0] == "0" && input[1] == "0" && input[3] == "3" && input[4] == "9" && input.Length == 14)
+        { return input + ""; }*/
+        return "";
+    }
+}
